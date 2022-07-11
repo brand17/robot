16bd38a001b4cb50a8784c33e3a43ea9dd5b70ff
brand17 <36546021+brand17@users.noreply.github.com>
2022-07-11 21:03:42 +0300

    move to one file

--- main/main.cpp
+++ main/main.cpp
@@ -21,18 +21,64 @@
 using Eigen::MatrixXd;
 
 MPU6050 mpu = MPU6050();
+bool mpuInterrupt = false;
 
 extern "C" {
 	void app_main(void);
 }
 
-extern void task_initI2C(void*);
-extern void task_display(void*);
-extern bool mpuInterrupt;
-
-static void IRAM_ATTR gpio_isr_handler(void* arg)
+void IRAM_ATTR gpio_isr_handler(void* arg)
 {
     mpuInterrupt = true;
+}
+
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+uint16_t packetSize = 42;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+
+void printAngles(){
+    mpuIntStatus = mpu.getIntStatus();
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+
+    // otherwise, check for DMP data ready interrupt frequently)
+    } else if (mpuIntStatus & 0x02) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetGravity(&gravity, &q);
+        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+        printf("YAW: %3.1f, ", ypr[0] * 180/M_PI);
+        printf("PITCH: %3.1f, ", ypr[1] * 180/M_PI);
+        printf("ROLL: %3.1f \n", ypr[2] * 180/M_PI);
+    }
+}
+
+void task_display(void*){
+	while(1){
+        if (mpuInterrupt){
+            mpuInterrupt = false;
+            printAngles();
+        }
+	    //Best result is to match with DMP refresh rate
+	    // Its last value in components/MPU6050/MPU6050_6Axis_MotionApps20.h file line 310
+	    // Now its 0x13, which means DMP is refreshed with 10Hz rate
+		vTaskDelay(5/portTICK_PERIOD_MS);
+	}
+
+	vTaskDelete(NULL);
 }
 
 void app_main(void)
