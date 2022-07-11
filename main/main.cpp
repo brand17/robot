0ebe293246f74fb5e22c5d2ec08f8df8a39bbae3
brand17 <36546021+brand17@users.noreply.github.com>
2022-07-11 08:29:08 +0300

    refactoring

--- main/main.cpp
+++ main/main.cpp
@@ -1,10 +1,23 @@
 #include "freertos/FreeRTOS.h"
 #include "sdkconfig.h"
 #include "freertos/task.h"
+
+#include <driver/i2c.h>
+#include <esp_log.h>
+#include <esp_err.h>
+#include "MPU6050.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#include "iot_servo.h"
+
+#define PIN_SDA 21
+#define PIN_CLK 22
+
 #include <iostream>
 #include <Eigen/Dense>
  
 using Eigen::MatrixXd;
+
+MPU6050 mpu = MPU6050();
 
 extern "C" {
 	void app_main(void);
@@ -22,7 +35,30 @@ void app_main(void)
     // m(1,1) = m(1,0) + m(0,1);
     // std::cout << m << std::endl;
 
-    xTaskCreate(&task_initI2C, "mpu_task", 2048, NULL, 5, NULL);
+    // xTaskCreate(&task_initI2C, "mpu_task", 2048, NULL, 5, NULL);
+	i2c_config_t conf;
+	conf.mode = I2C_MODE_MASTER;
+	conf.sda_io_num = (gpio_num_t)PIN_SDA;
+	conf.scl_io_num = (gpio_num_t)PIN_CLK;
+	conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+	conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+	conf.master.clk_speed = 400000;
+    conf.clk_flags = 0;
+	ESP_ERROR_CHECK(i2c_param_config(I2C_NUM_0, &conf));
+	ESP_ERROR_CHECK(i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0));
+	mpu.initialize();
+	mpu.dmpInitialize();
+
+	// This need to be setup individually
+	// mpu.setXGyroOffset(220);
+	// mpu.setYGyroOffset(76);
+	// mpu.setZGyroOffset(-85);
+	// mpu.setZAccelOffset(1788);
+    mpu.CalibrateAccel(6);
+    mpu.CalibrateGyro(6);
+
+	mpu.setDMPEnabled(true);
+
     // task_initI2C(NULL);
     vTaskDelay(500/portTICK_PERIOD_MS);
     // task_display(NULL);
